a8c5a621f0c7de51ab0cc5975bb5718dc4fd6ae3
Matt Buchanan <mbkodex42@hotmail.com>
2021-02-20 16:27:31 -0500

    Added slf4j for detailed logging.

--- src/main/java/com/comp3000/project/cms/HelloWorld.java
+++ src/main/java/com/comp3000/project/cms/HelloWorld.java
@@ -1,7 +1,14 @@
 package com.comp3000.project.cms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloWorld {
+
+    private final static Logger log = LoggerFactory.getLogger(HelloWorld.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        log.info("Hello Info");
+        log.debug("Hello Debug");
     }
 }
