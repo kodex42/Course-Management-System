dbfb1b85bb0025c305caba94cd4f21fbed78dace
Matt Buchanan <mbkodex42@hotmail.com>
2021-02-20 19:00:52 -0500

    Swapped HelloWorld.java for Main.java and imported Spring-Context

---
+++ core/src/main/java/com/comp3000/project/cms/Main.java
@@ -0,0 +1,13 @@
+package com.comp3000.project.cms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Course Management System started");
+    }
+}
